716dbf8882c6c52185db0f29c1ce1cf52045f4c2
kimkwanka <kimkwanka@gmail.com>
2021-08-19 11:42:30 +0200

    fix: only use navigation.setOption once component has mounted

--- components/ChatScreen.jsx
+++ components/ChatScreen.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
 
 const ChatScreen = ({ navigation, route: { params: { name, bgCol } } }) => {
-  navigation.setOptions({ title: name });
+  useEffect(() => {
+    navigation.setOptions({ title: name });
+  }, []);
+
   return (
     <View style={{
       flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: bgCol,
